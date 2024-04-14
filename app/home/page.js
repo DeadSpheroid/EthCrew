a62bc33bd3e02f2d544d3c3b17a9313b1ef8b80e
TIDYMOUSE <eulerandfriends@gmail.com>
2024-04-14 06:14:21 +0530

    sidebar added -0

--- app/home/page.js
+++ app/home/page.js
@@ -1,70 +1,83 @@
-"use client"
-import { useEffect, useState } from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import Map from '@/components/Map';
-import Link from 'next/link';
+"use client";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Map from "@/components/Map";
+import Link from "next/link";
+import SideBar from "@/components/Sidebar";
+import { SidebarItem } from "@/components/Sidebar";
 // import { auth } from '../firebase';
 // import { onAuthStateChanged, signOut } from 'firebase/auth';
 // import { useRouter } from 'next/router';
 
 export default function Home() {
+  //   const [user, setUser] = useState(null);
+  //   const router = useRouter();
 
-//   const [user, setUser] = useState(null);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     return onAuthStateChanged(auth, user => {
-//       if (user) {
-//         setUser({
-//           name: user.displayName,
-//           photoUrl: user.photoURL,
-//         });
-//       } else {
-//         setUser(null);
-//         router.push('/login');
-//       }
-//     })
-//   }, []);
+  //   useEffect(() => {
+  //     return onAuthStateChanged(auth, user => {
+  //       if (user) {
+  //         setUser({
+  //           name: user.displayName,
+  //           photoUrl: user.photoURL,
+  //         });
+  //       } else {
+  //         setUser(null);
+  //         router.push('/login');
+  //       }
+  //     })
+  //   }, []);
 
   return (
-    <div className='flex flex-col h-screen'>
-      <Map />
-      <div className='flex-1 p-4' >
-        <div className='flex justify-between items-center'>
+    <>
+      <div className="flex flex-row">
+        <SideBar>
+          <SidebarItem text="hello" />
+        </SideBar>
 
-          <img className='h-28' src='https://i.ibb.co/ZMhy8ws/uber-logo.png' />
+        <div className="flex flex-col h-screen w-full">
+          <Map />
+          <div className="flex-1 p-4">
+            <div className="flex justify-between items-center">
+              <img
+                className="h-28"
+                src="https://i.ibb.co/ZMhy8ws/uber-logo.png"
+              />
+            </div>
+            <div className="flex">
+              <div className="flex flex-col flex-1 bg-gray-200 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl">
+                <Link href="/search">
+                  <img
+                    className="h-3/5"
+                    src="https://i.ibb.co/cyvcpfF/uberx.png"
+                  />
+                  Ride
+                </Link>
+              </div>
 
+              <div className="flex flex-col flex-1 bg-gray-200 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl">
+                <img
+                  className="h-3/5"
+                  src="https://i.ibb.co/n776JLm/bike.png"
+                />
+                Wheels
+              </div>
 
-
-        </div>
-        <div className='flex'>
-
-            <div className='flex flex-col flex-1 bg-gray-200 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl'>
-          <Link href='/search' >
-              <img className='h-3/5' src='https://i.ibb.co/cyvcpfF/uberx.png' />
-              Ride
-          </Link>
+              <div className="flex flex-col flex-1 bg-gray-200 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl">
+                <img
+                  className="h-3/5"
+                  src="https://i.ibb.co/5RjchBg/uberschedule.png"
+                />
+                Reserve
+              </div>
             </div>
 
-          <div className='flex flex-col flex-1 bg-gray-200 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl'>
-            <img className='h-3/5' src='https://i.ibb.co/n776JLm/bike.png' />
-            Wheels
+            <div className="h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8">
+              Where to
+            </div>
           </div>
-
-          <div className='flex flex-col flex-1 bg-gray-200 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl'>
-            <img className='h-3/5' src='https://i.ibb.co/5RjchBg/uberschedule.png' />
-            Reserve
-          </div>
-          
         </div>
-
-        <div className='h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8'>
-          Where to
-        </div>
-
       </div>
-    </div>
-  )
-};
-
+    </>
+  );
+}
